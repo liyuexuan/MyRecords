9b8f195c9f5750a002020b90d74252d92821b6e7
debbili <liyuexuan25@163.com>
2023-11-09 17:21:19 +0800

    init project base files
    Signed-off-by: debbili <liyuexuan25@163.com>

---
+++ app/src/main/java/debbili/myrecords/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,22 @@
+package debbili.myrecords.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
